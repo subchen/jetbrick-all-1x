31d0f62db392e7c906d3fc8faff9587e39c70727
subchen <subchen@gmail.com>
2014-05-23 21:38:43 +0800

    Update RequestIntrospectUtils

--- jetbrick-commons/src/main/java/jetbrick/web/servlet/RequestIntrospectUtils.java
+++ jetbrick-commons/src/main/java/jetbrick/web/servlet/RequestIntrospectUtils.java
@@ -20,53 +20,65 @@ package jetbrick.web.servlet;
 
 import java.util.*;
 import javax.servlet.ServletRequest;
-import jetbrick.beans.introspectors.ClassDescriptor;
-import jetbrick.beans.introspectors.PropertyDescriptor;
 import jetbrick.lang.StringUtils;
+import jetbrick.lang.Validate;
+import jetbrick.reflect.KlassInfo;
+import jetbrick.reflect.PropertyInfo;
 import jetbrick.typecast.TypeCastUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
+/**
+ * 将 Request 中的参数，全部注入到一个 form 对象中.
+ * 
+ * @author Guoqiang Chen
+ */
 public abstract class RequestIntrospectUtils {
-    protected static final Logger log = LoggerFactory.getLogger(RequestIntrospectUtils.class);
 
-    public static void introspect(Object form, ServletRequest request) {
-        ClassDescriptor desc = ClassDescriptor.lookup(form.getClass());
-        for (Map.Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
-            String name = entry.getKey();
-            try {
-                PropertyDescriptor pd = desc.getPropertyDescriptor(name);
-                if (pd == null) continue;
+    @SuppressWarnings("unchecked")
+    public static <T> T introspect(Class<T> formClass, ServletRequest request) {
+        Validate.notNull(formClass);
 
-                String values[] = entry.getValue();
-                if (values == null) continue;
-
-                Class<?> type = pd.getRawType();
-                if (type.isArray()) {
-                    Class<?> componentType = type.getComponentType();
-                    Object data = TypeCastUtils.convertToArray(values, componentType);
-                    pd.invokeSetter(form, data);
-                } else if (type == List.class || type == Collection.class) {
-                    Class<?> componentType = pd.getRawComponentType(0);
-                    Object data = TypeCastUtils.convertToList(values, componentType);
-                    pd.invokeSetter(form, data);
-                } else {
-                    String value = StringUtils.trimToNull(values[0]);
-                    if (value == null) {
-                        if (type.isPrimitive()) {
-                            continue;
-                        } else {
-                            pd.invokeSetter(form, null);
-                        }
-                    } else {
-                        Object data = TypeCastUtils.convert(value, type);
-                        pd.invokeSetter(form, data);
-                    }
-                }
-            } catch (Throwable e) {
-                log.warn("Can't set property for Object.", e);
-            }
-        }
+        KlassInfo klass = KlassInfo.create(formClass);
+        T form = (T) klass.newInstance();
+        return introspect(form, request);
     }
 
+    public static <T> T introspect(T form, ServletRequest request) {
+        Validate.notNull(form);
+        Validate.notNull(request);
+
+        Class<? extends Object> formClass = form.getClass();
+        KlassInfo klass = KlassInfo.create(formClass);
+        for (Map.Entry<String, String[]> entry : request.getParameterMap().entrySet()) {
+            String name = entry.getKey();
+            PropertyInfo prop = klass.getProperty(name);
+            if (prop == null) continue;
+
+            String values[] = entry.getValue();
+            if (values == null) continue;
+
+            Class<?> type = prop.getRawType(formClass);
+            if (type.isArray()) {
+                Class<?> componentType = type.getComponentType();
+                Object data = TypeCastUtils.convertToArray(values, componentType);
+                prop.set(form, data);
+            } else if (type == List.class || type == Collection.class) {
+                Class<?> componentType = prop.getRawComponentType(formClass, 0);
+                Object data = TypeCastUtils.convertToList(values, componentType);
+                prop.set(form, data);
+            } else {
+                String value = StringUtils.trimToNull(values[0]);
+                if (value == null) {
+                    if (type.isPrimitive()) {
+                        continue;
+                    } else {
+                        prop.set(form, null);
+                    }
+                } else {
+                    Object data = TypeCastUtils.convert(value, type);
+                    prop.set(form, data);
+                }
+            }
+        }
+        return form;
+    }
 }
