e0262e34db9210b46968c71282e3511b3e9acf5e
subchen <subchen@gmail.com>
2014-05-24 19:21:03 +0800

    Add unit test for KlassInfo

--- jetbrick-commons/src/test/java/jetbrick/reflect/KlassInfoTest.java
+++ jetbrick-commons/src/test/java/jetbrick/reflect/KlassInfoTest.java
@@ -18,7 +18,8 @@
  */
 package jetbrick.reflect;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.HashMap;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -37,80 +38,5 @@ public class KlassInfoTest {
 
         Assert.assertEquals(k3, k1.getSuperKlass());
         Assert.assertEquals(3, k1.getInterfaces().size());
-    }
-
-    @Test
-    public void getDeclaredConstructors() {
-        KlassInfo klass = KlassInfo.create(HashMap.class);
-        List<ConstructorInfo> constructors = klass.getDeclaredConstructors();
-        Assert.assertEquals(4, constructors.size());
-    }
-
-    @Test
-    public void getDeclaredConstructor() {
-        KlassInfo klass = KlassInfo.create(HashMap.class);
-        Assert.assertNotNull(klass.getDefaultConstructor());
-        Assert.assertNotNull(klass.getDeclaredConstructor());
-        Assert.assertNotNull(klass.getDeclaredConstructor(int.class));
-        Assert.assertNotNull(klass.getDeclaredConstructor(Map.class));
-
-        Assert.assertNull(klass.getDeclaredConstructor(long.class));
-        Assert.assertNotNull(klass.searchDeclaredConstructor(long.class));
-    }
-
-    @Test
-    public void getDeclaredMethods() {
-        KlassInfo klass = KlassInfo.create(Object.class);
-        Assert.assertEquals(12, klass.getDeclaredMethods().size());
-    }
-
-    @Test
-    public void getDeclaredMethodsWithFilter() {
-        KlassInfo k1 = KlassInfo.create(Object.class);
-        List<MethodInfo> methods = k1.getDeclaredMethods(Filters.PUBLIC_METHOD);
-        Assert.assertEquals(9, methods.size());
-
-        KlassInfo k2 = KlassInfo.create(UUID.class);
-        methods = k2.getDeclaredMethods(Filters.STATIC_METHOD);
-        Assert.assertEquals(4, methods.size());
-    }
-
-    @Test
-    public void getDeclaredMethod() {
-        KlassInfo klass = KlassInfo.create(Map.class);
-        Assert.assertNotNull(klass.getDeclaredMethod("get", Object.class));
-        Assert.assertNotNull(klass.getDeclaredMethod("put", Object.class, Object.class));
-
-        Assert.assertNull(klass.getDeclaredMethod("get"));
-        Assert.assertNull(klass.getDeclaredMethod("get", String.class));
-    }
-
-    @Test
-    public void getMethods() {
-        KlassInfo k1 = KlassInfo.create(UUID.class);
-        Assert.assertEquals(27, k1.getMethods().size());
-
-        KlassInfo k2 = KlassInfo.create(List.class);
-        Assert.assertEquals(47, k2.getMethods().size());
-    }
-
-    @Test
-    public void getMethodsWithFilter() {
-        KlassInfo klass = KlassInfo.create(UUID.class);
-        List<MethodInfo> methods = klass.getMethods(Filters.PUBLIC_METHOD);
-        Assert.assertEquals(24, methods.size());
-
-        methods = klass.getMethods(Filters.INSTANCE_METHOD);
-        Assert.assertEquals(23, methods.size());
-    }
-
-    @Test
-    public void getMethod() {
-        KlassInfo klass = KlassInfo.create(HashMap.class);
-        Assert.assertNotNull(klass.getMethod("get", Object.class));
-        Assert.assertNotNull(klass.getMethod("toString"));
-
-        Assert.assertNull(klass.getMethod("get", String.class));
-        Assert.assertNotNull(klass.searchMethod("get", String.class));
     }
 }
